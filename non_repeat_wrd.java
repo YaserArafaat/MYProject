efa40a552b312f090de1f62046fd0da0ae9e3186
YaserArafaat <yasu.arfaat@gmail.com>
2016-08-07 11:57:30 +0530

    Create non_repeat_wrd.java

---
+++ non_repeat_wrd.java
@@ -0,0 +1,39 @@
+package guvi;
+
+
+import java.util.Scanner;
+
+public class non_repeat_wrd {
+public static void main(String[] args) {
+	System.out.println("enter the length of array : ");	
+	Scanner in=new Scanner(System.in);
+	
+	int len=in.nextInt();
+	int []arr=new int[len];
+	System.out.println("enter the elements one by one : ");
+	for(int i=0;i<len;i++)
+	{
+		arr[i]=in.nextInt();
+	}
+	for(int i=0;i<len;i++)
+	{
+		for(int j=i+1;j<len;j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				arr[i]='@';
+				arr[j]='@';
+				break;
+				
+			}
+		}
+	}
+	for(int i=0;i<len;i++)
+	{
+		if(arr[i]!='@')
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
+}
