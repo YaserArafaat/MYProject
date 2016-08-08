2c4ba75e3fd62d20bd352c9107db2bce5c642ae5
YaserArafaat <yasu.arfaat@gmail.com>
2016-08-08 10:18:20 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,31 @@
+package guvi;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+	System.out.println("Enter the string : ");	
+	String input=s.nextLine();
+	char [] arr=input.toCharArray();
+	LinkedHashSet<Character> dup=new LinkedHashSet<Character>();
+	for(int i=0;i<arr.length;i++)
+	{
+		dup.add(arr[i]);
+	}
+	String res="";
+	Iterator<Character> it=dup.iterator();
+	while(it.hasNext())
+	{
+		res += it.next();
+	}
+	System.out.println("String after removing duplicates : ");
+	System.out.println(res);
+	
+	}
+
+}
