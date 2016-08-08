678aaaeb5fceaa75379baf7630835d770a98e1ac
YaserArafaat <yasu.arfaat@gmail.com>
2016-08-08 10:23:38 +0530

    Create to_find_target_num.java

---
+++ to_find_target_num.java
@@ -0,0 +1,39 @@
+package guvi;
+
+
+import java.util.Scanner;
+
+public class to_find_target_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n;
+		System.out.println("Enter size of array");
+		n=s.nextInt();
+		int[] arr=new int[n];
+		System.out.println("Enter elements");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		System.out.println("Enter the Target number : ");
+		int target=s.nextInt();
+		int a=0;
+		int b=0;
+		for(int i=0;i<arr.length;i++)
+		{
+		     for(int j=0;j<arr.length;j++)
+		     {
+		    	 if(arr[i]+arr[j]==target)
+		    	 {
+		    		 a=arr[i];
+		    		 b=arr[j];
+		    	 }
+		     }
+		}
+		
+		System.out.println("the elements whose sum produce target value : "+a+" "+b);
+	}
+
+}
